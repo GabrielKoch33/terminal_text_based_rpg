d5be5ab4b7382f59cd0308b8f3dddc8c9e592fde
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-09-22 14:44:24 -0300

    loja agora é final

--- src/main/java/org/gabriel/classesMecanicas/Loja.java
+++ src/main/java/org/gabriel/classesMecanicas/Loja.java
@@ -8,7 +8,7 @@ public class Loja {
     /**
     * Sempre declaramos a generalização (List), depois inicializamos a especificação (ArrayList, LinkedList, ...)
     **/
-    private List<Item> loja;
+    private final List<Item> loja;
 
     public Loja() {
         this.loja = new ArrayList<>();
@@ -18,11 +18,6 @@ public class Loja {
         this.loja.add(item);
     }
 
-    /**
-    * Em Sets, o método '.remove()' retorna um boolean. <br>
-    * Já em Maps ou Lists, o retorno é o próprio Objeto (ou null caso a chave não exista).<br>
-    * O parâmetro passado para o remove é a chave, o retorno do remove() é o valor associado.
-    **/
     public Item removeItemEstoque(int index){
         return loja.remove(index);
     }
